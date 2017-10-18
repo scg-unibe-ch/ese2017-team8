c5bdaa38f5a649aa74c83ca4f2ed280f47e44bdd
Samuel Schwegler <samuel.schwegler@gmx.ch>
2017-10-18 11:14:25 +0200

    improved the date format and added a few comments to OrderController

--- src/main/java/main/OrderController.java
+++ src/main/java/main/OrderController.java
@@ -9,15 +9,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * handles the order creation form
+ */
+
 @Controller
 public class OrderController {
 
     @Autowired
     public OrderRepo orderRepo;
 
-    Date now = new Date();
-
-    Order bsp1 = new Order("Kreissäge",2,32, now,3L);
+    //just an example
+    Order bsp1 = new Order("Kreissäge",2,32, new Date(102,11,4),3L);
 
     @RequestMapping(value="/logistics", method= RequestMethod.GET)
     public String orderForm(Model model) {
@@ -26,6 +29,10 @@ public class OrderController {
         return "logistics";
     }
 
+    /**
+     * handles a form with post method
+     * @return direction of post output
+     */
     @RequestMapping(value="/logistics", method=RequestMethod.POST)
     public String orderSubmit(@ModelAttribute Order order, Model model) {
         model.addAttribute("order", order);
@@ -34,6 +41,10 @@ public class OrderController {
         return "result";
     }
 
+    /**
+     * is neccessary for the thymeleaf table representation of the data
+     * @return list with all orders
+     */
     @ModelAttribute("allOrders")
     public List<Order> showAllOrders() {
         return this.orderRepo.findAll();
