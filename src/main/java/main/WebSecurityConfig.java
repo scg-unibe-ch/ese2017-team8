e7c6d43dcf31f75df27772299f5403bac4078c7e
Samuel Schwegler <samuel.schwegler@gmx.ch>
2017-10-24 21:50:29 +0200

    added stylesheet

--- src/main/java/main/WebSecurityConfig.java
+++ src/main/java/main/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/home", "/stylesheets/**").permitAll()
 				.antMatchers("/logistics/**").hasRole("LOGISTICIAN")
 				.antMatchers("/driver/**").hasRole("DRIVER")
 				.and()
